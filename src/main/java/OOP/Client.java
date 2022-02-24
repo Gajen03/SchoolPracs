a8e4754b3347e938fdbf20ac90b109c679b4ae4a
Gajendran <Gajendran@ZAKZRHB-CLLB16.ballito.local>
2022-02-24 12:41:55 +0200

    OOP getters and setteer bank client program

---
+++ src/main/java/OOP/Client.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package OOP;
+
+/**
+ *
+ * @author Gajendran
+ */
+public class Client {
+    
+    private String clientname = "";
+    private int accontNumber = 0;
+    private int balance = 0;
+    
+    public Client(){
+        clientname = "Mason Mount";
+        accontNumber = 191008;
+        balance = 100000;
+    }
+    
+    public void newName(String newName){
+        clientname = newName;
+    }
+    
+//    public static String changeName(){
+//        
+//     
+//        
+//    }
+    public void newAccountNumber(String newAccountNumber){
+        clientname = newAccountNumber;
+    }
+    
+    public void newBalance(String newBalance){
+        clientname = newBalance;
+    }
+     
+    
+   
+    
+}
