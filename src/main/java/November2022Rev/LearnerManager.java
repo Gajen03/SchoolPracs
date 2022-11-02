3178b0a685bacf48bc7b91f26bcc6fa45d11cd9e
Gajendran <Gajendran@ZAKZRHB-CLLB16.ballito.local>
2022-11-02 08:16:23 +0200

    done up till 4.4 in Term4 Past Paper

--- src/main/java/November2022Rev/LearnerManager.java
+++ src/main/java/November2022Rev/LearnerManager.java
@@ -15,35 +15,53 @@ import java.util.Scanner;
  * @author Gajendran
  */
 public class LearnerManager {
-    
+
     private static Learner learnerArr[] = new Learner[50];
-    int learnerCount = 0; 
+    int learnerCount = 0;
 
     public LearnerManager() throws FileNotFoundException {
         File f = new File("data\\excursion.txt");
-        
+
         Scanner fileSc = new Scanner(f);
-        
-       
+
         DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-                
-        while(fileSc.hasNext()){
+
+        while (fileSc.hasNext()) {
             Scanner sc = new Scanner(fileSc.nextLine()).useDelimiter("#");
             String name = sc.next();
-            
+
             String activity1 = sc.next();
             double activity1Cost = sc.nextDouble();
             LocalDateTime activity1Date = LocalDateTime.parse(sc.next(), inputFormatter);
-                    
-                    
+
             String activity2 = sc.next();
             double activity2Cost = sc.nextDouble();
             LocalDateTime activity2Date = LocalDateTime.parse(sc.next(), inputFormatter);
-            
+
+        }
+    }
+
+    public void overrideLearnerCode() {
+        for (int i = 0; i < learnerArr.length; i++) {
+            Learner learner = learnerArr[i];
+            String learnerCode = learner.getLearnerCode() + "" + i;
+            i++;
+
         }
     }
     
-    
-    
-    
+    public void sort(){
+        
+        for (int sorted = learnerArr.length-1;sorted>0; sorted--) {
+            for(int currentIndex =0;currentIndex<sorted;currentIndex++ ){
+                Learner learner = learnerArr[currentIndex];
+                double totalCost = learner.calcStudentTotalCost();
+                if(totalCost){
+                    
+                }
+            }
+            
+        }
+    }
+
 }
