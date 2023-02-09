322a6c3f2e038861b510a027da000f7b5fe52890
Gajendran <Gajendran@ZAKZRHB-CLLB16.ballito.local>
2023-02-09 11:39:40 +0200

    Past Paper

--- src/main/java/Inheritance/CrewMemberManager.java
+++ src/main/java/Inheritance/CrewMemberManager.java
@@ -4,10 +4,72 @@
  */
 package Inheritance;
 
+import java.time.LocalDate;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 /**
  *
  * @author Gajendran
  */
 public class CrewMemberManager {
+
+    private CrewMember[] cArr = new CrewMember[80];
+    private int size = 0;
+
+    public CrewMemberManager() throws FileNotFoundException {
+        File f = new File("data\\crewmembers.txt");
+
+        Scanner sc = new Scanner(f);
+
+        while (sc.hasNext()) {
+            Scanner lineSc = new Scanner(sc.nextLine()).useDelimiter("#");
+            String fullname = lineSc.next();
+            int crewID = lineSc.nextInt();
+            String department = lineSc.next();
+
+            cArr[size] = new CrewMember(fullname, crewID, department);
+            size++;
+        }
+
+    }
     
+    public int findCrewMeber(int crewID){
+        int max = size-1;
+        int min = 0;
+        int mid = (max + min) / 2;
+
+        while (min <= max) {
+            if (crewID < mid) {
+                min = mid + 1;
+            }
+            else if(crewID >mid){
+                max = mid-1;
+            }else{
+                return mid;
+            } 
+        }
+        return -1;
+    }
+    
+    // you were on 6.2 
+    
+     
+
+
+    public String toString() {
+
+        String output = "";
+        for (int i = 0; i < size; i++) {
+            output += cArr[i];
+            output += "\n";
+        }
+        return output;
+    }
+
+
+      
+      
 }
---
+++ src/main/java/Inheritance/CrewMemberUI.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Inheritance;
+
+import java.io.FileNotFoundException;
+
+/**
+ *
+ * @author Gajendran
+ */
+public class CrewMemberUI {
+    public static void main(String[] args) throws FileNotFoundException {
+        
+        CrewMemberManager cmm = new CrewMemberManager();
+        
+        System.out.println(cmm.toString());
+        
+    }
+}
